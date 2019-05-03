14bf547805caea4b776b1b16b0bd3b40e3b3435f
Ivan Boychuk <boychuk.m.ivan@gmail.com>
2019-05-03 15:56:44 -0500

    Added NuGet Packages Created the Project Structure

---
+++ AutomationFinalProject/Configuration/Config.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFinalProject.Configuration
+{
+
+    public class Config
+    {
+        public static string GetEnvironment()
+        {
+            return GetConfigValue("Environment");
+        }
+        public static string GetBrowser()
+        {
+            return GetConfigValue("Browser");
+        }
+        public static string GetUrl()
+        {
+            string key = "" + GetEnvironment();
+            return GetConfigValue(key);
+        }
+        public static string GetConfigValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
---
+++ AutomationFinalProject/WebDriver/DriverUtils.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomationFinalProject.Configuration;
+
+namespace AutomationFinalProject.WebDriver
+{
+    class DriverUtils
+    {
+        public static IWebDriver CreateWebDriver()
+        {
+            string browser = Config.GetBrowser();
+            if (browser == "Chrome")
+            {
+                return new ChromeDriver();
+            }
+            else if (browser == "FireFox")
+            {
+                return new FirefoxDriver();
+            }
+            else
+            {
+                throw new InvalidOperationException("Browser is not supported");
+            }
+        }
+    }
+}
